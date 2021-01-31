b40588e56a8f691b7b0bf6db6eb1eda71f1f9ec7
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2021-01-31 15:07:21 -0800

    Add Initial Files for iOS Repository

---
+++ iOS/megastonks/megastonks/megastonksApp.swift
@@ -0,0 +1,17 @@
+//
+//  megastonksApp.swift
+//  megastonks
+//
+//  Created by Kingsley Okeke on 2021-01-31.
+//
+
+import SwiftUI
+
+@main
+struct megastonksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
