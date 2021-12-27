71a458684bbc23a40e9aae26e1034e5754da9e8c
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2021-12-26 23:44:51 -0800

    Clean up previous application and initialize project genesis At this point, this is a brand new SwiftUI project

---
+++ iOS/megastonks/megastonks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  megastonks
+//
+//  Created by Kingsley Okeke on 2021-12-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
