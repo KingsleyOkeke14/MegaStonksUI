74d095492e4cf1b7e1bc0f6da94c1776100d61c5
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2022-01-03 14:40:48 -0800

    Add Poppins Font Add Transparent Logo Add Login View

---
+++ iOS/megastonks/megastonks/Styles/Colors.swift
@@ -0,0 +1,21 @@
+//
+//  Colors.swift
+//  megastonks
+//
+//  Created by Kingsley Okeke on 2022-01-03.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static var megaStonksLightGreen: Color {.init(.megaStonksLightGreen)}
+    static var megaStonksDarkGreen: Color {.init(.megaStonksDarkGreen)}
+    static var megaStonksLightGray: Color {.init(.megaStonksLightGray)}
+}
+
+extension UIColor {
+    static var megaStonksLightGreen: UIColor { .init(red: 0.14, green: 0.24, blue: 0.16, alpha: 1) }
+    static var megaStonksDarkGreen: UIColor { .init(red: 0.0, green: 0.12, blue: 0.05, alpha: 1) }
+    static var megaStonksLightGray: UIColor { .init(red: 0.33, green: 0.33, blue: 0.33, alpha: 1) }
+}
