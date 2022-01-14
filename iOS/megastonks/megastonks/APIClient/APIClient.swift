f7cd80208dfc000700cd08915e97ebbb5b5163c3
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2022-01-13 23:07:30 -0800

    Add API Error, API Request, and getData call that would be used for all get requests

--- iOS/megastonks/megastonks/APIClient/APIClient.swift
+++ iOS/megastonks/megastonks/APIClient/APIClient.swift
@@ -6,9 +6,9 @@
 //
 
 import Foundation
-import UIKit
+import Combine
 
-struct APIClient {
+class APIClient {
     
     struct APIRoutes {
         let server:String
@@ -32,5 +32,54 @@ struct APIClient {
         }
     }
     
-    var routes: APIRoutes
+    struct APIRequest {
+        var request:URLRequest
+        
+        init(url:URL, httpMethod:String, authToken: String = "") {
+            request = URLRequest(url: url)
+            request.httpMethod = httpMethod
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            request.setValue("Bearer \(authToken)", forHTTPHeaderField: "Authorization")
+        }
+    }
+    
+    enum APIError: Error {
+        case invalidClient
+        case invalidURL
+        case responseError
+        case unknown
+    }
+    
+    let shared = APIClient()
+    var routes: APIRoutes = APIRoutes()
+    private var cancellables = Set<AnyCancellable>()
+    
+    func getData<T: Decodable>(request: APIRequest, type: T.Type) -> Future<[T], Error> {
+         return Future<[T], Error> { [weak self] promise in
+             guard let self = self else { return promise(.failure(APIError.invalidClient)) }
+             URLSession.shared.dataTaskPublisher(for: request.request)
+                 .tryMap { (data, response) -> Data in
+                     guard let httpResponse = response as? HTTPURLResponse, 200...299 ~= httpResponse.statusCode else {
+                         throw APIError.responseError
+                     }
+                     return data
+                 }
+                 .decode(type: [T].self, decoder: JSONDecoder())
+                 .receive(on: RunLoop.main)
+                 .sink(receiveCompletion: { (completion) in
+                     if case let .failure(error) = completion {
+                         switch error {
+                         case let decodingError as DecodingError:
+                             promise(.failure(decodingError))
+                         case let apiError as APIError:
+                             promise(.failure(apiError))
+                         default:
+                             promise(.failure(APIError.unknown))
+                         }
+                     }
+                 }, receiveValue: { promise(.success($0)) })
+                 .store(in: &self.cancellables)
+         }
+     }
 }
